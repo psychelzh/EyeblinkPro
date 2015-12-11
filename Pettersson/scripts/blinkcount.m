08465958aaf2d5ac8f5c3e38754327764070e645
Liang Zhang <psychelzh@gmail.com>
2015-12-11 13:30:16 +0800

    More statistics output for debugging and optimizing.

--- Pettersson/scripts/blinkcount.m
+++ Pettersson/scripts/blinkcount.m
@@ -1,4 +1,4 @@
-function blink = blinkcount(EOG, sr, display)
+function [numBlk, stat] = blinkcount(EOG, sr, display)
 %BLINKCOUNT counts eye blinks in the EOG data. When set 'on' to display
 %variable, a figure is plotted to denote all the eye blinks found in the
 %data.
@@ -7,10 +7,9 @@ function blink = blinkcount(EOG, sr, display)
 %   Add one more output, 11/21/2015, Zhang Liang.
 %   Structurize the output into a structure variable, 'blink'. 12/10/2015,
 %   Zhang Liang. E-mail:psychelzh@gmail.com
+%   Number of blink will be an instinct output now.
 
 %Preallocating.
-blink = struct('blinknum', {}, 'blinkpeak', {}, 'stat', {});
-
 minargs = 2;
 maxargs = 3;
 narginchk(minargs, maxargs);
@@ -22,18 +21,16 @@ end
 maxima = findLocalMaxima(EOG);
 localmax = (sort(maxima) - min(maxima)) / (max(maxima) - min(maxima));
 %S2:Determine the threshold value of telling the noise and blink peak apart in the data. 
-[val, gof] = deAmpThreshVal(localmax, 10);
+[val, stat] = deAmpThreshVal(localmax, 2);
 dp = val * (max(maxima) - min(maxima)) + min(maxima);
 %S3:Find out each peak and its duration.
 [loc, LB, RB] = dePeak(EOG, sr, dp, 400);
 
 %Generating output.
 numBlk = length(loc);
-blink(1).blinknum = numBlk;
-blink(1).blinkpeak = loc;
-blink(1).stat.gof = gof;
-blink(1).stat.LB = LB;
-blink(1).stat.RB = RB;
+stat.blinkpeak = loc;
+stat.LB = LB;
+stat.RB = RB;
 
 %Plot part.
 if strcmp(display,'on')
--- Pettersson/scripts/deAmpThreshVal.m
+++ Pettersson/scripts/deAmpThreshVal.m
@@ -1,5 +1,9 @@
-function [val, gof] = deAmpThreshVal(vec, start)
+function [val, stat] = deAmpThreshVal(vec, start)
 %DEAMPTHRESHVAL is exclusively used in finding the blink analysis.
+%   Statistics are included in output stat. Now contains three fields: 
+%   ind: index of the most suitable fitness.
+%   gof: the minimun of sse.
+%   wgof: the maximum of sse.
 
 % Initiated by Zhang, Liang. 3/26/2015.
 minargs = 1;
@@ -8,13 +12,19 @@ narginchk(minargs, maxargs);
 if nargin == 1
     start = 10;
 end
-sse = inf(1, length(vec));
-for i = start:length(vec) - 1
+num = length(vec);
+sse = inf(1, num);
+for i = start:num - 1
     Dn = vec(1:i);
     Dp = vec(i:end);
     [~, gofn] = fit((1:i)', double(Dn'), 'poly1');
-    [~, gofp] = fit((i:length(vec))', double(Dp'), 'poly1');
+    [~, gofp] = fit((i:num)', double(Dp'), 'poly1');
     sse(i) = gofn.sse + gofp.sse;
 end
-[gof, ind] = min(sse);
+[ssemin, ind] = min(sse);
+ssemax = max(sse(start:num - 1));
 val = vec(ind);
+stat.ind = ind;
+stat.num = num;
+stat.gof = ssemin;
+stat.wgof = ssemax;
