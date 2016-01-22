4aab0fc9f83f29bfbc6bb5b24ce345e8c7b60d33
Zhang Liang <psychelzh@gmail.com>
2016-01-22 11:26:23 +0800

    Add more task setting files.

---
+++ Pettersson/scripts/ts_RLBT.m
@@ -0,0 +1,21 @@
+%Clear original tasksettting.
+clear tasksetting
+
+%Settings of the task Filtering.
+tasksetting.datapath              = 'E:\EEG_Gene_Data\EEG_Results'; %Specify where to read the data.
+tasksetting.dataprefix              = 'RLB'; %Specify which data to read.
+tasksetting.datasuffix              = 'T';
+
+%For trial definition.
+tasksetting.trialpar.continuous   = 'yes';
+tasksetting.trialpar.trigger      = [1 2]; 
+% tasksetting.trialpar.trialprepost = [-1, 60];
+tasksetting.trialpar.starttime    = 0;
+tasksetting.trialpar.channel      = {'EXG3' 'EXG4' 'EXG5' 'EXG6'};
+tasksetting.trialpar.minevent     = 48;
+
+%Specify where to store data.
+cur_path = fileparts(mfilename('fullpath'));
+tasksetting.outpath               = [fileparts(cur_path), filesep, 'ResData'];
+% Specify data prefix.
+tasksetting.outdataprefix         = 'EOG_RLBT';
