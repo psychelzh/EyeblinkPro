6a6119a49175b48b31be99b46c44606ac2ac9752
Liang Zhang <psychelzh@outlook.com>
2019-04-18 23:00:07 +0800

    Save EOG variable only

--- Extract_EOG.m
+++ Extract_EOG.m
@@ -4,6 +4,6 @@ tasks = {'FLT', 'REST', 'RLA', 'RLAT', 'RLB', 'RLBT', 'RLB', 'RLBT', 'SM', 'TEST
 for i_task = 1:length(tasks)
     task = tasks{i_task};
     EOG = extract_eog(task);
-    save(fullfile('EOG', sprintf('EOG_%s', task)))
+    save(fullfile('EOG', sprintf('EOG_%s', task)), 'EOG')
 end
 rmpath scripts
