0591b01ad67e986ddc30f4fa14b299309c00ef7e
Liang Zhang <psychelzh@outlook.com>
2019-04-18 13:42:01 +0800

    Rearrange files

--- Wilson/Threshold.m
+++
@@ -1,5 +0,0 @@
-function postThreshEOG = Threshold(postMedianEOG, W)
-%THRESHOLD performs the threshold filtering of the threshold of W.
-
-postThreshEOG = postMedianEOG;
-postThreshEOG(abs(postThreshEOG) < W) = 0;
