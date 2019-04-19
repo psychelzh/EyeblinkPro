92d43e7cebc0e8f8d1dd2e2b7bc0f0a9579389f6
Liang Zhang <psychelzh@outlook.com>
2019-04-19 17:53:23 +0800

    Fix an issue of `qsub` by specifying nodes config

--- matlab_jobs.sh
+++ matlab_jobs.sh
@@ -4,7 +4,7 @@ for task in "FLT" "REST" "RLA" "RLAT" "RLB" "RLBT" "SM" "TEST"
 do
     printf "restoredefaultpath\naddpath ~/zhangliang/toolbox/fieldtrip-20190416/\nExtract_EOG('$task');\nexit;" > $task.m
     printf "/opt/software/MATLAB/R2017a/bin/matlab -nodisplay < $task.m" > $task.sh
-    qsub -d /brain/guixue/geneproject/EEG/EEG_Gene_Data -N ${task}_EBR $task.sh
+    qsub -l nodes=1:ppn=12 -d /brain/guixue/geneproject/EEG/EEG_Gene_Data -N ${task}_EBR -q long $task.sh
     rm $task.m
     rm $task.sh
 done
