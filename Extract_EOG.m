ac44021bcc4dcf40746fbaf5e75dd253ed0086ec
Liang Zhang <psychelzh@outlook.com>
2019-04-20 10:46:35 +0800

    Modify the saving version to `-v7.3`

--- Extract_EOG.m
+++ Extract_EOG.m
@@ -1,5 +1,5 @@
 function Extract_EOG(taskname)
 addpath scripts
 EOG = extract_eog(taskname);
-save(fullfile('EOG', sprintf('EOG_%s', taskname)), 'EOG')
+save(fullfile('EOG', sprintf('EOG_%s', taskname)), 'EOG', '-v7.3', '-nocompression')
 rmpath scripts
