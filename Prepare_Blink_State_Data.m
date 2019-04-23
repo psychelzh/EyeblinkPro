2e7ff398739f97d8fb30c0cb7ca4f9489c9f2b89
Liang Zhang <psychelzh@outlook.com>
2019-04-23 14:51:33 +0800

    Add preliminary codes for blink state analysis

---
+++ Prepare_Blink_State_Data.m
@@ -0,0 +1,97 @@
+% Prepare dataset for future analysis in R language
+%
+% This script is used to transform matlab's complex data structure to an
+% easy to read tabular data and output it to a tsv format file.
+
+% TODO: transform this to a function to analyze every task
+addpath scripts
+% merge EOG data and calculated blink results
+data_path = 'EOG';
+taskname = 'FLT';
+% get the trigger mapping of value and name
+tasksetting = get_config(taskname);
+trigger_map_config = struct2table(tasksetting.triggermap);
+trigger_map_table = stack(trigger_map_config, 1:width(trigger_map_config), ...
+    'IndexVariableName', 'name', 'NewDataVariableName', 'key');
+trigger_map = containers.Map(mod(trigger_map_table.key, 16) * 16 + 15, ...
+    cellstr(trigger_map_table.name));
+load(fullfile(data_path, sprintf('blink_res_%s', taskname)))
+load(fullfile(data_path, sprintf('EOG_%s', taskname)))
+data_this_task = outerjoin(struct2table(EOG), blink_res, ...
+    'Keys', 'pid', 'MergeKeys', true, ...
+    'LeftVariables', {'pid', 'fsample', 'event', 'EOGv'}, ...
+    'RightVariables', {'pid', 'stat'});
+num_subj = height(data_this_task);
+event = table;
+for i_subj = 1:num_subj
+    event_subj = data_this_task.event{i_subj};
+    stat_subj = data_this_task.stat{i_subj};
+    EOGv_subj = data_this_task.EOGv{i_subj};
+    if isempty(event_subj)
+        continue
+    end
+    % map event value to real event name
+    event_subj.sample = [];
+    event_subj.name = strings(height(event_subj), 1);
+    event_subj_has_name = isKey(trigger_map, num2cell(event_subj.value));
+    event_subj.name(event_subj_has_name) = ...
+        values(trigger_map, num2cell(event_subj.value(event_subj_has_name)));
+    % blink related events
+    event_blink = table;
+    blink_fields = {'LB', 'blinkpeak', 'RB'};
+    blink_event_names = string(strcat('blink_', {'start', 'peak', 'end'}));
+    for i_trl = 1:length(stat_subj)
+        event_blink_trial = table;
+        for i_blk_fld = 1:length(blink_fields)
+            blink_field = blink_fields{i_blk_fld};
+            blink_event_name = blink_event_names(i_blk_fld);
+            event_blink_field = table( ...
+                repmat({'BLINK'}, length(stat_subj(i_trl).(blink_field)), 1), ...
+                EOGv_subj.trial{i_trl}(3, stat_subj(i_trl).(blink_field))', ...
+                repmat(i_trl, length(stat_subj(i_trl).(blink_field)), 1), ...
+                EOGv_subj.time{i_trl}(stat_subj(i_trl).(blink_field))', ...
+                repmat(blink_event_name, length(stat_subj(i_trl).(blink_field)), 1), ...
+                'VariableNames', {'type', 'value', 'trl', 'time', 'name'});
+            event_blink_trial = cat(1, event_blink_trial, event_blink_field);
+        end
+        event_blink = cat(1, event_blink, event_blink_trial);
+    end
+    event_subj = cat(1, event_subj, event_blink);
+    event_subj = sortrows(event_subj, {'trl', 'time'});
+    % add trial id to events
+    event_subj.trial = nan(height(event_subj), 1);
+    trial_id = 0;
+    is_in_trial = false;
+    for i_event = 1:height(event_subj)
+        name_this_event = event_subj.name(i_event);
+        if startsWith(name_this_event, 'stim')
+            if trial_id == 0
+                trial_id = trial_id + 1;
+            else
+                if ~isempty(tasksetting.trialdur)
+                    trial_id = trial_id + ...
+                        round((event_subj.time(i_event) - time_last_stim) / ...
+                        tasksetting.trialdur);
+                else
+                    trial_id = trial_id + 1;
+                end
+            end
+            time_last_stim = event_subj.time(i_event);
+            is_in_trial = true;
+        end
+        if is_in_trial
+            event_subj.trial(i_event) = trial_id;
+        end
+        if startsWith(name_this_event, 'resp')
+            is_in_trial = false;
+        end
+        if startsWith(name_this_event, 'start')
+            trial_id = 0;
+        end
+    end
+    event_subj.pid = repmat(data_this_task.pid(i_subj), height(event_subj), 1);
+    event_subj = event_subj(:, {'pid', 'trl', 'trial', 'type', 'name', 'value', 'time'});
+    event = cat(1, event, event_subj);
+end
+writetable(event, fullfile(data_path, sprintf('event_%s.txt', taskname)), 'Delimiter', '\t')
+rmpath scripts
