22d0eec42cf3579adc8ed8b9f720f7bbb34ba8d1
Liang Zhang <psychelzh@outlook.com>
2019-04-27 21:44:56 +0800

    Update blink merging by adding repaired data

--- Merge_Blink.m
+++ Merge_Blink.m
@@ -2,19 +2,34 @@
 % The minimal version of MATLAB to run this script should be R2018a
 %
 % All rights are reserved. 2019 (c) Liang Zhang <psychelzh@outlook.com>
+
 data_path = 'EOG';
-data_files = dir(fullfile(data_path, 'blink_res_*.mat'));
-blink_res = [];
-for i_file = 1:length(data_files)
-    data_file_name = data_files(i_file).name;
-    taskname = regexp(data_file_name, '(?<=blink_res_)\w+', 'match', 'once');
-    data_file_content = load(fullfile(data_path, data_file_name));
-    blink_res_single = data_file_content.blink_res;
-    blink_res_single = addvars(blink_res_single, ...
-        repmat(string(taskname), height(blink_res_single), 1), ...
-        'After', 1, 'NewVariableNames', 'task_name');
-    blink_res = cat(1, blink_res, blink_res_single);
+log_path = 'logs';
+tasknames = {'FLT', 'REST', 'RLA', 'RLAT', 'RLB', 'RLBT', 'SM', 'TEST'};
+blink_res = cell(length(tasknames), 1);
+for i_task = 1:length(tasknames)
+    taskname = tasknames{i_task};
+    data_suffix = {'', '_repaired'};
+    blink_res_task = cell(length(data_suffix), 1);
+    for i_suffix = 1:length(data_suffix)
+        blink_res_raw = load(fullfile(data_path, ...
+            sprintf('blink_res_%s%s', taskname, data_suffix{i_suffix})));
+        check_res = readtable(fullfile(log_path, ...
+            sprintf('check_results_%s%s.txt', taskname, data_suffix{i_suffix})));
+        blink_res_finally = blink_res_raw.blink_res;
+        if ismember('Recheck', check_res.Properties.VariableNames)
+            blink_res_finally(ismember(check_res.Recheck, -2:0), :) = [];
+        else
+            blink_res_finally(ismember(check_res.Message, -2:0), :) = [];
+        end
+        blink_res_task{i_suffix} = blink_res_finally;
+    end
+    blink_res_task = cat(1, blink_res_task{:});
+    blink_res_task = addvars(blink_res_task, ...
+        repmat({taskname}, height(blink_res_task), 1), ...
+        'NewVariableNames', 'task');
+    blink_res_task = blink_res_task(:, {'task', 'pid', 'rate_blink'});
+    blink_res{i_task} = blink_res_task;
 end
-save(fullfile(data_path, 'blink_res'), 'blink_res')
-EBR = unstack(blink_res(:, {'pid', 'task_name', 'rate_blink'}), 'rate_blink', 'task_name');
-writetable(EBR, fullfile(data_path, 'EBR.txt'), 'Delimiter', '\t')
+blink_res = unstack(cat(1, blink_res{:}), "rate_blink", "task");
+writetable(blink_res, fullfile(log_path, 'blink_res.xlsx'))
